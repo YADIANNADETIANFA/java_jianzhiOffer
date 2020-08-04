66635b54dffcf4937dd8f84c7dc3c8f580bd16c2
YADIANNADETIANFA <871227769@qq.com>
2020-08-05 00:38:04 +0800

    提交单例

---
+++ 单例/src/DemoApplication.java
@@ -0,0 +1,9 @@
+
+public class DemoApplication {
+    public static void main(String[] args){
+        Singleton1 singleton1 = Singleton1.getInstance();
+        Singleton2 singleton2 = Singleton2.getInstance();
+        Singleton3 singleton3 = Singleton3.getInstance();
+    }
+}
+
